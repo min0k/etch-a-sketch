42d6dcca9aefd850aab7c8120f6ed8ae9ee2a0ec
min0k <mino.kim@minokim.com>
2021-11-15 16:50:51 -0800

    add small and big grid functionality

--- js.js
+++ js.js
@@ -1,24 +1,63 @@
 
-let body = document.querySelector(".container");
+const body = document.querySelector(".container");
+const button = document.querySelector("button");
+
+const smallGrid = document.querySelector(".small-grid");
+const bigGrid = document.querySelector(".big-grid");
+
+let allDivs;
+let gridSize = 256;
+
+button.addEventListener("click", clear);
+
+bigGrid.addEventListener("click", clearAndUpdateSizeBig);
+smallGrid.addEventListener("click", clearAndUpdateSizeSmall);
+
+createGrid(gridSize);
+
+function createGrid(size) {
+    if (size === 256) {
+        body.style.gridTemplateColumns = `repeat(16, 1fr)`;
+        body.style.gridTemplateRows = `repeat(16, 1fr)`;
+    } else if (size === 1024) {
+        body.style.gridTemplateColumns = `repeat(32, 1fr)`;
+        body.style.gridTemplateRows = `repeat(32, 1fr)`;
+    }
+    for (let i = 1; i <= size; i++) {
+        const gridDiv = document.createElement("div");
+        gridDiv.className = `gridDiv${i} grids`;
+        gridDiv.addEventListener("mouseover", changeColor);
+        body.appendChild(gridDiv);
+    }
+}
+
+
+allDivs = document.querySelectorAll(".grids");
+
+function clearAndUpdateSizeSmall(ee) {
+    allDivs = document.querySelectorAll(".grids");
+    allDivs.forEach(function (e) {
+        e.remove();
+    });
+    createGrid(256);
+}
+    
+function clearAndUpdateSizeBig(ee) {
+    allDivs = document.querySelectorAll(".grids");
+    allDivs.forEach(function (e) {
+        e.remove();
+    });
+    createGrid(1024);
+}
 
 function changeColor(e) {
     e.target.style.backgroundColor = "black";
 }
 
-for (let i = 1; i <= 256; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.className = `gridDiv${i} grids`;
-    gridDiv.addEventListener("mouseover", changeColor);
-    body.appendChild(gridDiv);
-}
-
-let allDivs = document.querySelectorAll(".grids");
-let button = document.querySelector("button");
-
-button.addEventListener("click", clear);
-
 function clear() {
+    allDivs = document.querySelectorAll(".grids");
     allDivs.forEach(function (e) {
         e.style.backgroundColor = "white";
     });
 }
+
